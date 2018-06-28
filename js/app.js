5d6f1d59d79ccd2cc3a93a8de8d767302f0b1958
Prejith Premkumar <prajithpremg@gmail.com>
2018-06-28 23:23:01 +0530

    add smooth scrol

--- js/app.js
+++ js/app.js
@@ -26,7 +26,9 @@ TxtType.prototype.tick = function () {
     var that = this;
     var delta = 200 - Math.random() * 100;
 
-    if (this.isDeleting) { delta /= 2; }
+    if (this.isDeleting) {
+        delta /= 2;
+    }
 
     if (!this.isDeleting && this.txt === fullTxt) {
         delta = this.period;
@@ -59,22 +61,28 @@ document.body.appendChild(css);
 
 //Custom fadeIn function other than the one provided by jQuery, taken from StackOverflow
 function fadeIn(el, duration, display) {
-    var s = el.style, step = 25 / (duration || 300);
+    var s = el.style,
+        step = 25 / (duration || 300);
     s.opacity = s.opacity || 0;
     s.display = display || "block";
-    (function fade() { (s.opacity = parseFloat(s.opacity) + step) > 0.6 ? s.opacity = 0.6: setTimeout(fade, 25); })();
+    (function fade() {
+        (s.opacity = parseFloat(s.opacity) + step) > 0.6 ? s.opacity = 0.6 : setTimeout(fade, 25);
+    })();
 }
 
 //Custom fadeOut function other than the one provided by jQuery, taken from StackOverflow
 function fadeOut(el, duration) {
-    var s = el.style, step = 25 / (duration || 300);
+    var s = el.style,
+        step = 25 / (duration || 300);
     s.opacity = s.opacity || 1;
-    (function fade() { (s.opacity -= step) < 0 ? s.display = "none" : setTimeout(fade, 25); })();
+    (function fade() {
+        (s.opacity -= step) < 0 ? s.display = "none" : setTimeout(fade, 25);
+    })();
 }
 
 //Helper functions for class manipulations
 function hasClass(el, className) {
-    return el.classList ? el.classList.contains(className) : new RegExp('\\b'+ className+'\\b').test(el.className);
+    return el.classList ? el.classList.contains(className) : new RegExp('\\b' + className + '\\b').test(el.className);
 }
 
 function addClass(el, className) {
@@ -84,23 +92,30 @@ function addClass(el, className) {
 
 function removeClass(el, className) {
     if (el.classList) el.classList.remove(className);
-    else el.className = el.className.replace(new RegExp('\\b'+ className+'\\b', 'g'), '');
+    else el.className = el.className.replace(new RegExp('\\b' + className + '\\b', 'g'), '');
 }
 
 let contact = document.querySelector('.contact');
 let contactPopup = document.querySelector('.contact-popup-outter');
-contact.addEventListener('click', function() {
+contact.addEventListener('click', function () {
     fadeIn(contactPopup, 1000, 'flex');
 });
 
 let cross = document.querySelector('.close-cross');
-cross.addEventListener('click', function() {
+cross.addEventListener('click', function () {
     fadeOut(contactPopup, 1000);
 });
 
-document.addEventListener('keyup', function(evt) {
+document.addEventListener('keyup', function (evt) {
     evt = evt || window.event;
-    if(evt.keyCode === 27) {
+    if (evt.keyCode === 27) {
         fadeOut(contactPopup, 1000);
     }
+});
+
+document.querySelector('#screen-2-link').addEventListener('click', function (event) {
+    event.preventDefault();
+    document.querySelector('#screen-2').scrollIntoView({
+        behavior: 'smooth'
+    });
 });
